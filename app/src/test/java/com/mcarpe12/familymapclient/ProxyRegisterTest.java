f2e0d260ae0cab78313c1e0eae61a08ba73d1ce2
mcarpe12 <mcarpe12@byu.edu>
2019-12-10 16:57:21 -0700

    Updated Register Tests

--- app/src/test/java/com/mcarpe12/familymapclient/ProxyRegisterTest.java
+++ app/src/test/java/com/mcarpe12/familymapclient/ProxyRegisterTest.java
@@ -6,6 +6,7 @@ import com.mcarpe12.familymapclient.service.Proxy;
 import org.junit.Test;
 
 import java.net.URL;
+import java.util.UUID;
 
 import familymap.Person;
 import request.RegisterRequest;
@@ -13,11 +14,11 @@ import response.RegisterResponse;
 
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertNull;
 
 public class ProxyRegisterTest {
     private String SERVER_URL = "http://localhost:8080";
-    private final String USERNAME = "bob";
     private final String PASSWORD = "parker";
     private final String EMAIL = "bparker@email.com";
     private final String FIRST_NAME = "Bob";
@@ -29,8 +30,11 @@ public class ProxyRegisterTest {
         String registerURL = SERVER_URL + "/user/register";
         URL url = new URL(registerURL);
 
+        String userName = UUID.randomUUID().toString();
+        userName = userName.substring(0, 8);
+
         RegisterRequest registerReq = new RegisterRequest(
-                USERNAME, PASSWORD, EMAIL, FIRST_NAME, LAST_NAME, GENDER
+                userName, PASSWORD, EMAIL, FIRST_NAME, LAST_NAME, GENDER
         );
         RegisterResponse registerRes = Proxy.register(url, registerReq);
         DataCache.getInstance().setAuthToken(registerRes.getAuthToken());
@@ -52,22 +56,25 @@ public class ProxyRegisterTest {
 
     @Test
     public void register_invalid() throws Exception {
-        String taken_username = "sheila";
+        String userName = UUID.randomUUID().toString();
+        userName = userName.substring(0, 8);
 
         String registerURL = SERVER_URL + "/user/register";
         URL url = new URL(registerURL);
 
-        // Should return null: username is already taken
+        // Register new user
         RegisterRequest registerReq = new RegisterRequest(
-                taken_username, PASSWORD, EMAIL, FIRST_NAME, LAST_NAME, GENDER
+                userName, PASSWORD, EMAIL, FIRST_NAME, LAST_NAME, GENDER
         );
         RegisterResponse registerRes = Proxy.register(url, registerReq);
         DataCache.getInstance().setAuthToken(registerRes.getAuthToken());
-        String token = registerRes.getAuthToken();
-        String personID = registerRes.getPersonID();
+
+        // Register with same credentials -- should get error
+        registerRes = Proxy.register(url, registerReq);
 
         assertNotNull(registerRes);
-        assertNull(token);
-        assertNull(personID);
+        String message = registerRes.getMessage();
+        boolean error = message.contains("error");
+        assertTrue(error);
     }
 }
